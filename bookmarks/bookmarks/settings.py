112e6da3cf2e49d43c171b1cba02ff0a9db34814
tomasrpita <tomasrpita@gmail.com>
2024-07-08 21:05:44 +0200

    chore:  configure media settings, and add profile edit functionality

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -131,3 +131,7 @@ LOGOUT_URL = "logout"
 # Email settings
 # Print emails to the console
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
--- bookmarks/bookmarks/urls.py
+++ bookmarks/bookmarks/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -22,3 +24,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("account/", include("account.urls")),
 ]
+
+if settings.DEBUG:
+    # Dont use django to serve media files in production
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
