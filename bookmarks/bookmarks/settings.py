f6745d31abb5b6ab978053fee6a13cfc69b228f8
tomasrpita <tomasrpita@gmail.com>
2024-07-08 05:43:38 +0200

    chore: Enable password reset functionality and update related templates

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -127,3 +127,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_REDIRECT_URL = "dashboard"
 LOGIN_URL = "login"
 LOGOUT_URL = "logout"
+
+# Email settings
+# Print emails to the console
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
