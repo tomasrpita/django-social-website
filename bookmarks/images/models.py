ab68215c431bf698e6e9c400b1c3d1ca412fba31
tomasrpita <tomasrpita@gmail.com>
2024-07-10 05:48:38 +0200

    chore: Add users_like field to Image model for user liking functionality

--- bookmarks/images/models.py
+++ bookmarks/images/models.py
@@ -16,6 +16,10 @@ class Image(models.Model):
     description = models.TextField(blank=True)
     created = models.DateTimeField(auto_now_add=True)
 
+    users_like = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name="images_liked", blank=True
+    )
+
     class Meta:
         indexes = [
             models.Index(fields=["-created"]),
