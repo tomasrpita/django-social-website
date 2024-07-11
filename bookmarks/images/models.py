cf794acb105c3c5cf6f67579b728484d88d08acf
tomasrpita <tomasrpita@gmail.com>
2024-07-11 06:36:18 +0200

    chore: Add image detail view and template for displaying image details

--- bookmarks/images/models.py
+++ bookmarks/images/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.db import models
 from django.utils.text import slugify
+from django.urls import reverse
 
 
 class Image(models.Model):
@@ -30,6 +31,9 @@ class Image(models.Model):
         if not self.slug:
             self.slug = slugify(self.title)
         super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse("images:detail", args=[self.id, self.slug])
 
     def __str__(self):
         return self.title
