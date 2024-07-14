7994e928e661e8c1773bbbe999b639ebfa4a78f2
tomasrpita <tomasrpita@gmail.com>
2024-07-14 10:12:39 +0200

    feat: Add create_action function to register and image_create views

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,3 +1,4 @@
+from actions.utils import create_action
 from django.contrib import messages
 from django.contrib.auth import authenticate, get_user_model, login
 from django.contrib.auth.decorators import login_required
@@ -48,6 +49,7 @@ def register(request):
             new_user.save()
             # Create the user profile
             Profile.objects.create(user=new_user)
+            create_action(new_user, "has created an account")
             return render(request, "account/register_done.html", {"new_user": new_user})
     else:
         user_form = UserRegistrationForm()
@@ -106,6 +108,7 @@ def user_follow(request):
             user = User.objects.get(id=user_id)
             if action == "follow":
                 Contact.objects.get_or_create(user_from=request.user, user_to=user)
+                create_action(request.user, "is following", user)
             else:
                 Contact.objects.filter(user_from=request.user, user_to=user).delete()
             return JsonResponse({"status": "ok"})
