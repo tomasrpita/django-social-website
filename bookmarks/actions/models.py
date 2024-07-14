2c8f5ae311016dd21f64580399928ac7ce8211ed
tomasrpita <tomasrpita@gmail.com>
2024-07-14 09:49:57 +0200

    chore: Add admin interface for Action model, update Action model with target field and add utils create_action function

--- bookmarks/actions/models.py
+++ bookmarks/actions/models.py
@@ -1,4 +1,6 @@
 from django.conf import settings
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
 
@@ -9,8 +11,19 @@ class Action(models.Model):
     verb = models.CharField(max_length=255)
     created = models.DateTimeField(auto_now_add=True)
 
+    target_ct = models.ForeignKey(
+        ContentType,
+        blank=True,
+        null=True,
+        related_name="target_obj",
+        on_delete=models.CASCADE,
+    )
+    target_id = models.PositiveIntegerField(null=True, blank=True)
+    target = GenericForeignKey("target_ct", "target_id")
+
     class Meta:
         indexes = [
             models.Index(fields=["-created"]),
+            models.Index(fields=["target_ct", "target_id"]),
         ]
         ordering = ["-created"]
