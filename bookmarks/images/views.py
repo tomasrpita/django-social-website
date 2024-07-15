7cd8d48e8c570de42303302b5a69a089e5120889
tomasrpita <tomasrpita@gmail.com>
2024-07-15 06:17:41 +0200

    chore: Add Redis configuration for caching image views

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -1,4 +1,6 @@
+import redis
 from actions.utils import create_action
+from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
@@ -8,6 +10,10 @@ from django.views.decorators.http import require_POST
 
 from .forms import ImageCreateForm
 from .models import Image
+
+r = redis.Redis(
+    host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.REDIS_DB
+)
 
 
 @login_required
@@ -37,8 +43,12 @@ def image_create(request):
 @login_required
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
+    # increment total image views by 1
+    total_views = r.incr(f"image:{image.id}:views")
     return render(
-        request, "images/image/detail.html", {"section": "images", "image": image}
+        request,
+        "images/image/detail.html",
+        {"section": "images", "image": image, "total_views": total_views},
     )
 
 
